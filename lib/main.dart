73e8c28944bc071efed63c30b9c2f09686bb5554
Denzel <chiagozieani145@gmail.com>
2019-10-26 12:11:01 +0100

    So i added images to the background cool!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Harigato People"),
-      centerTitle: true,
-      backgroundColor: Colors.black,
-    ),
-    body: Center(
-      child:  Text(
-          "Hello Ninja's",
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.normal,
-          color: Colors.black45,
+  home: Home()
+  ),
+);
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Harigato People"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child:  Image(
+          image: AssetImage('assets/wallpaper6.jpg'),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(onPressed: null ,
-      child: Text("Click"),
-      backgroundColor: Colors.black,
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(onPressed: null ,
+        child: Text("Click"),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
